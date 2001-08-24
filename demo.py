eabcfc8cca0b995977bc8df40624597d90b7e1f1
Ryan Davis <ryand@zenspider.com>
2001-08-24 11:01:38 -0800

    Moved old python based ZenWeb stuff out.
    [git-p4: depot-paths = "//src/ZenWeb/dev/": change = 829]

--- demo.py
+++
@@ -1,9 +0,0 @@
-#!/bin/env python
-
-from ZSSWebsite import DemoSite
-
-# brought into __main__ for ZenWebsite
-from ZSSWebsite import ZSSDocument, ZSSSiteMap, ZSSRawDocument
-
-if __name__ == '__main__':
-	DemoSite("SiteMap", "./demo", "./demohtml").renderSite()
