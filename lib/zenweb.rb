1089f3228e8f15cff7d5d441904440aeed75f349
Ryan Davis <ryand@zenspider.com>
2012-01-19 17:55:16 -0800

    doco for zenweb.rb and config.rb [git-p4: depot-paths = "//src/ZenWeb/dev/": change = 6929]

--- lib/zenweb.rb
+++ lib/zenweb.rb
@@ -1,7 +1,11 @@
 require "rubygems"
 require "time"
 
+##
+# Don't look at me, I'm just a namespace! Look at Zenweb::Site or Zenweb::Page.
+
 module Zenweb
+  # duh
   VERSION = "3.0.0b1"
 end
 
--- lib/zenweb/config.rb
+++ lib/zenweb/config.rb
@@ -4,10 +4,21 @@ gem "rake"
 require "rake"
 
 module Zenweb
+  ##
+  # Provides a hierarchical dictionary made of yaml fragments and files.
+  #
+  # Any given page in zenweb can start with a YAML header. All files
+  # named "_config.yml" up the directory tree to the top are
+  # considered parents of that config. Access a config like you would
+  # any hash and you get inherited values.
+
   class Config
     include Rake::DSL
 
     attr_reader :site, :path, :parent
+
+    ##
+    # Create a new Config for site at a given path.
 
     def initialize site, path
       @site, @path, @parent = site, path, nil
@@ -21,15 +32,18 @@ module Zenweb
       @parent ||= Config::Null
     end
 
+    ##
+    # Access value at +k+. The value can be inherited from the parent configs.
+
     def [] k
       h[k] or parent[k]
     end
 
-    def h
+    def h # :nodoc:
       @h ||= YAML.load(File.read path) || {}
     end
 
-    def inspect
+    def inspect # :nodoc:
       if Rake.application.options.trace then
         "Config[#{path.inspect}, #{parent.inspect}, #{h.inspect[1..-2]}]"
       else
@@ -37,9 +51,12 @@ module Zenweb
       end
     end
 
-    def to_s
+    def to_s # :nodoc:
       "Config[#{path.inspect}]"
     end
+
+    ##
+    # Wire up this config to the rest of the rake dependencies.
 
     def wire
       @wired ||= false # HACK
@@ -54,11 +71,12 @@ module Zenweb
     end
   end # class Config
 
+  # :stopdoc:
   Config::Null = Class.new Config do
     def [] k;                    end
     def initialize;              end
     def inspect; "Config::Null"; end
     def wire;                    end
   end.new
+  # :startdoc:
 end
-
