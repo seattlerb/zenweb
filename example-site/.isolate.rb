8f076adbbb892f1c57b7d20f178a90c6dec6f700
Ryan Davis <ryand@zenspider.com>
2012-02-03 03:51:01 -0800

    Fixed yaml headers and added some unicode for testing [git-p4: depot-paths = "//src/ZenWeb/dev/": change = 7002]

--- example-site/.isolate.rb
+++ example-site/.isolate.rb
@@ -1,18 +1,6 @@
 require "isolate"
 
 Isolate.now! :path => ".isolate", :system => false do
-  gem 'rack'
-  # gem 'jekyll'
-  # gem 'rdiscount'
-  # gem 'pygments.rb'
-  # gem 'RedCloth'
-  # gem 'haml', '>= 3.1'
-  # gem 'compass', '>= 0.11'
-  # gem 'rubypants'
-  gem 'rb-fsevent'
-  # gem 'stringex'
-
-  # I know I'm using these:
   gem 'rake',     '~> 0.9'
   gem 'less',     '~> 1.2'
   gem 'coderay',  '~> 1.0'
